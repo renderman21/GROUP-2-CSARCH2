f1e240d3ea559608f0ce36c9745b1787e1c4eb68
sebdc <sebastienmvdc@gmail.com>
2023-11-26 02:39:05 +0800

    Moved comments and separated initSequence()

--- algo.py
+++ algo.py
@@ -1,61 +1,70 @@
+import random
 '''
 This file contains the algorithm of FA+LRU
 
 '''
 
-import random as r
-
 class FALRU:
-    def __init__(self, testCase:int, numBlocks:int) -> None:
-        """This initializes the FALRU class. Used for choosing which test case be utilized,
+
+    """ 
+        This initializes the FALRU class. Used for choosing which test case be utilized,
         and how many memory blocks the user requested 
 
-        Args
-            testCase (int): This is based on the user's choice of what test case will be used
-            numBlocks (int): This is the user's requested amount of memory blocks to be outputed
-            
-        """
+        testCase (int): This is based on the user's choice of what test case will be used
+        numBlocks (int): This is the user's requested amount of memory blocks to be outputed
+        
+    """
+    def __init__(self, testCase:int, numBlocks:int) -> None:
         self.testCase = testCase
         self.numBlocks = numBlocks
     
+
+    """This will initialize the sequence, depending on what the test cases had been inputted
+    
+    Returns:
+        list: This contains the sequence
+    """
     def initSequence(self) -> list:
-        """This will initialize the sequence, depending on what the test cases had been inputted
-        
-        Returns:
-            list: This contains the sequence
-        """
-        
-        retList = []
+        list = []
         match self.testCase:
-            #Sequential Sequencing
             case 0: 
-                upperLimit = self.numBlocks * 2
-                for i in range(0, 4):
-                    for a in range(0, upperLimit):
-                        retList.append(a)
-                
-                return retList
-            #Random Sequencing
+                list = self.sequentialSequence()
             case 1: 
-                upperLimit = self.numBlocks * 4
-                
-                for i in range(upperLimit):
-                    retList.append(r.randint(0, upperLimit))
-                return retList
-            #Mid-Repeat Blocks
+                list = self.randomSequence()
             case 2: 
-                firstLimit = self.numBlocks - 1
-                secondLimit = self.numBlocks * 2
-                
-                for j in range(0, 4):
-                    for i in range(0, firstLimit):
-                        retList.append(i)
-                    
-                    for a in range(1, secondLimit):
-                        retList.append(a)
-                
-                return retList
+                list = self.repeatSequence()
             
+        return list
+
+    def sequentialSequence(self):
+        list = []
+        upperLimit = self.numBlocks * 2
+        for i in range(0, 4):
+            for a in range(0, upperLimit):
+                list.append(a)
+        return list
+
+    def randomSequence(self):
+        list = []
+        upperLimit = self.numBlocks * 4
+        for i in range(upperLimit):
+            list.append(random.randint(0, upperLimit))
+        return list
+
+    def repeatSequence(self):
+        list = []
+        firstLimit = self.numBlocks - 1
+        secondLimit = self.numBlocks * 2
+        
+        for j in range(0, 4):
+            for i in range(0, firstLimit):
+                list.append(i)
+            
+            for a in range(1, secondLimit):
+                list.append(a)
+        
+        return list
+
     def mainAlgo(self) -> list:
         """This is the function that performs FA + LRU
 
@@ -66,7 +75,6 @@ class FALRU:
         #TODO: Complete this function
         pass
 
-test = FALRU(1,8)
+test = FALRU(2,8)
 
 print(test.initSequence())
-
