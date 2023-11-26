60066cf61966bd69adb352de73c6de59fd029ac0
sebdc <sebastienmvdc@gmail.com>
2023-11-26 22:10:52 +0800

    Added algorithm for simulation

--- sequence.py
+++ sequence.py
@@ -1,32 +1,15 @@
 import random
-'''
-This file contains the algorithm of FA+LRU
 
-'''
-
-class FALRU:
-
-    """ 
-        This initializes the FALRU class. Used for choosing which test case be utilized,
-        and how many memory blocks the user requested 
-
-        testCase (int): This is based on the user's choice of what test case will be used
-        numBlocks (int): This is the user's requested amount of memory blocks to be outputed
-        
-    """
-    def __init__(self, testCase:int, numBlocks:int) -> None:
-        self.testCase = testCase
+class Sequence:
+    '''
+        numBlocks are for memory Blocks
+    '''
+    def __init__(self, numBlocks:int) -> None:
         self.numBlocks = numBlocks
     
-
-    """This will initialize the sequence, depending on what the test cases had been inputted
-    
-    Returns:
-        list: This contains the sequence
-    """
-    def initSequence(self) -> list:
+    def initSequence(self, testCase) -> list:
         list = []
-        match self.testCase:
+        match testCase:
             case 0: 
                 list = self.sequentialSequence()
             case 1: 
@@ -55,26 +38,14 @@ class FALRU:
         list = []
         firstLimit = self.numBlocks - 1
         secondLimit = self.numBlocks * 2
-        
         for j in range(0, 4):
             for i in range(0, firstLimit):
                 list.append(i)
-            
             for a in range(1, secondLimit):
                 list.append(a)
-        
         return list
-
-    def mainAlgo(self) -> list:
-        """This is the function that performs FA + LRU
-
-        Returns:
-            list: This contains the cache block
-        """
-        
-        #TODO: Complete this function
-        pass
-
-test = FALRU(2,8)
-
-print(test.initSequence())
+    
+# sequence = Sequence(32)
+# list = sequence.initSequence(0) 
+# print( list )
+# print( f"\n{len(list)}")
